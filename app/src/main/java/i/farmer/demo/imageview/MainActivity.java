e3261d4b6b241688209e88916b64235362d2e5e5
bei <guodongxiao@xiake99.com>
2021-10-15 16:43:00 +0800

    换个实现思路

--- app/src/main/java/i/farmer/demo/imageview/MainActivity.java
+++ app/src/main/java/i/farmer/demo/imageview/MainActivity.java
@@ -2,10 +2,8 @@ package i.farmer.demo.imageview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 
-import i.farmer.widget.imageview.ImageLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageLayout.ImageView mImageView1 = findViewById(R.id.mImageView1);
-        mImageView1.setBorder(30, Color.parseColor("#ff0000"));
     }
 }
